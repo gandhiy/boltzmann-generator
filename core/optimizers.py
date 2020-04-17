8d91a7379b06a6e053ae026f39e6032c72f6afef
Yash <ygandhi100@gmail.com>
2020-04-16 22:59:05 -0600

    working tf training w/ decorators

--- core/optimizers.py
+++ core/optimizers.py
@@ -1,17 +1,72 @@
 import tensorflow as tf 
 
-from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+
 
 class getOpt:
-    def __init__(self, type):
-        self.type = type
-    def get_optimizer(self, parameters = None):
-        if(self.type == 'SGD'):
-            return SGD(parameters)
-        elif(self.type == 'Adam'):
-            return Adam(parameters)
-        elif(self.type == 'RMSprop'):
-            return RMSprop(parameters)
-        else:
-            raise ValueError("need to specificy a corrent optimizer type")
-            
+    def __init__(self):
+        pass
+    
+    def adam(self, lr=0.01, b1=0.99, b2=0.9, **kwargs):
+        opt = AdamOpt(lr, b1, b2, **kwargs)
+        return opt.optimizer()
+
+    def rmsprop(self, lr=0.01, rho=0.9, momentum=0.0, **kwargs):
+        opt = RMSPropOpt(lr, rho, momentum, **kwargs)
+        return opt.optimizer()
+
+    def sgd(self, lr=0.01, nesterov=False, **kwargs):
+        opt = SGDOpt(lr, nesterov, **kwargs)
+        return opt.optimizer()
+
+class optimizerInterface:
+    def __init__(self):
+        pass
+
+    def optimizer(self):
+        raise NotImplementedError
+
+class AdamOpt(optimizerInterface):
+    def __init__(self, learning_rate, beta_1, beta_2, **kwargs):
+        super(AdamOpt, self).__init__()
+        self.lr = learning_rate
+        self.b1 = beta_1
+        self.b2 = beta_2
+        self.kwargs = kwargs
+
+    def optimizer(self):
+        return tf.keras.optimizers.Adam(
+            learning_rate=self.lr, 
+            beta_1=self.b1,
+            beta_2=self.b2,
+            **self.kwargs
+            )
+
+class RMSPropOpt(optimizerInterface):
+    def __init__(self, learning_rate, rho, momentum, **kwargs):
+        super(RMSPropOpt, self).__init__()
+        self.lr = learning_rate
+        self.rho = rho
+        self.mt = momentum
+        self.kwargs = kwargs
+
+    def optimizer(self):
+        return tf.keras.optimizers.RMSprop(
+            learning_rate=self.lr,
+            rho=self.rho,
+            momentum=self.mt,
+            **self.kwargs 
+        )
+    
+class SGDOpt(optimizerInterface):
+    def __init__(self, learning_rate, nesterov, **kwargs):
+        super(SGDOpt, self).__init__()
+        self.lr = learning_rate
+        self.nesterov = nesterov
+        self.kwargs = kwargs
+
+    def optimizer(self):
+        return tf.keras.optimizers.SGD(
+            learning_rate=self.lr,
+            nesterov=self.nesterov,
+            **self.kwargs
+        )
--- core/trainer.py
+++ core/trainer.py
@@ -1,54 +1,39 @@
+import os
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import tensorflow_probability as tfp
 
-
-from data import gen_double_moon_samples
-from networks import RealNVP
-
-from pdb import set_trace as debug
 
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
 
+
 class Trainer:
-    def __init__(self, configs, verbose=0):
-        self.model = RealNVP(**configs)
-        
-        if(verbose==1):
+    def __init__(self, model, targets, verbose=0):
+        self.model = model
+        self.verbose = verbose
+        if(self.verbose==1):
             print(self.model.summary())
         
-        _targets = gen_double_moon_samples(10000)
-        _targets = tf.random.shuffle(_targets)
+        
+        _targets = tf.random.shuffle(targets)
         self.train_dataset = (
             tf.data.Dataset.from_tensor_slices(_targets)
-            .shuffle(5000).batch(5000)
+            .shuffle(10000).batch(256)
         )
 
+        self.epoch = 0
+        self.iteration = 0
 
-    def _test_function(self):
-        pass
+
 
     def train(self, epochs):
-        for epoch in range(epochs):
-            for target in self.train_dataset:
-                self.model.train(target)
+        for epoch in range(epochs):            
+            self.epoch = epoch
+            for i, target in enumerate(self.train_dataset):
+                self.iteration += 1 
+                self.model.train(target, (self.epoch, self.iteration, i))
 
 
 
-
-
-if __name__ == "__main__":
-    t = Trainer({
-        'chain_length': 4,
-        'in_shape': [2],
-        'loss': 'basic',
-        'loss_parameters': {
-            'c1': 1.0
-        },
-        'optimizer': 'Adam',
-        'loc': [0.5, -2.5],        
-    })
-    t.train(10)
